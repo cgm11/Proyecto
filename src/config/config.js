e73db1f9f7827cb128d1696be596e196947f9604
cgm11 <caro.garcia0506@gmail.com>
2019-07-31 03:57:54 +0200

    cambio APY-key correos

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,3 @@
 process.env.PORT = process.env.PORT || 3000;
 
 process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/asignaturas';
-
-process.env.SENDGRID_API_KEY='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'
